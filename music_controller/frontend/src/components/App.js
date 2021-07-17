2d6fb4d5da075fb303ec4a242c6a237d7bf5e309
Darshill-Shah <darshilshah1231@gmail.com>
2021-07-17 16:34:44 +0530

    Integrating react and django (3)

---
+++ music_controller/frontend/src/components/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+
+// export default class App extends Component {
+//     constructor(props) {
+//         super(props);
+//     }
+
+//     render() {
+//         return (
+//             <h1>Testing React Code</h1>
+//         )
+//     }
+// }
+
+export default function App() {
+    return (
+        <h1>Testing React Code</h1>
+    )
+}
+
+const appDiv = document.getElementById("app");
+render(<App />, appDiv);
