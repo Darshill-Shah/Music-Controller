5b1d121b8d49c6245182794d478932c20b4c1ca7
Darshill-Shah <darshilshah1231@gmail.com>
2021-07-20 22:29:47 +0530

    Calling API endpoint from React (7) Function

--- music_controller/frontend/src/components/Room.js
+++ music_controller/frontend/src/components/Room.js
@@ -1,45 +1,91 @@
-import React, { Component } from "react";
+import React, { Component, useState, useEffect } from "react";
 
-export default class Room extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      votesToSkip: 3,
-      guestCanPause: false,
-      isHost: false,
-    };
-    this.roomCode = this.props.match.params.roomCode;
-    this.getRoomDetails();
-  }
+// export default class Room extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       votesToSkip: 3,
+//       guestCanPause: false,
+//       isHost: false,
+//     };
+//     this.roomCode = this.props.match.params.roomCode;
+//     this.getRoomDetails();
+//   }
 
-  getRoomDetails() {
-    fetch("/api/get-room" + "?code=" + this.roomCode)
+//   getRoomDetails() {
+// fetch("/api/get-room" + "?code=" + this.roomCode)
+//   .then((response) => {
+//     console.log(response);
+//     return response.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//     this.setState({
+//       votesToSkip: data.votes_to_skip,
+//       guestCanPause: data.guest_can_pause.toString(),
+//       isHost: data.is_host.toString(),
+//     });
+//   });
+//   }
+
+//   render() {
+//     // return <p>This is the home page</p>;
+//     return (
+//       <div>
+//         <h2>Room: {this.roomCode}</h2>
+//         <p>votesToSkip: {this.state.votesToSkip}</p>
+//         <p>guestCanPause: {this.state.guestCanPause}</p>
+//       </div>
+//     );
+//   }
+// }
+
+export default function Room(props) {
+  const [roomState, setRoomState] = useState({
+    votesToSkip: 3,
+    guestCanPause: "false",
+    isHost: "false",
+  });
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("/api/get-room" + "?code=" + props.match.params.roomCode)
       .then((response) => {
         console.log(response);
         return response.json();
       })
       .then((data) => {
         console.log(data);
-        this.setState({
+        setRoomState({
           votesToSkip: data.votes_to_skip,
           guestCanPause: data.guest_can_pause.toString(),
           isHost: data.is_host.toString(),
         });
+        setIsLoading(false);
+      })
+      .catch(() => {
+        setRoomState({
+          votesToSkip: "DATA NOT FOUND",
+          guestCanPause: "DATA NOT FOUND",
+          isHost: "DATA NOT FOUND",
+        });
+        setIsLoading(false);
       });
-  }
+  }, [props?.match?.params?.roomCode]);
 
-  render() {
-    // return <p>This is the home page</p>;
+  if (isLoading) {
     return (
-      <div>
-        <h2>Room: {this.roomCode}</h2>
-        <p>votesToSkip: {this.state.votesToSkip}</p>
-        <p>guestCanPause: {this.state.guestCanPause}</p>
-      </div>
+      <section>
+        <p>Loading...</p>
+      </section>
     );
   }
+  return (
+    <div>
+      <h2>Room: {props.match.params.roomCode}</h2>
+      <p>votesToSkip: {roomState.votesToSkip}</p>
+      <p>guestCanPause: {roomState.guestCanPause}</p>
+      <p>isHost: {roomState.isHost}</p>
+    </div>
+  );
 }
-
-// export default function RoomJoinPage() {
-//   return <p>This is the RoomJoinPage from Function</p>;
-// }
